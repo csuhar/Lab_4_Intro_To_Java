12735abaa372d97f88a92241e9c2d14d69878d73
Christopher Suhar <suharce@mail.uc.edu>
2024-09-23 20:04:31 -0400

    Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,45 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int intOperandA = 5;
+        int intOperandB = 15;
+        int Product = 75;
+        int Sum = 20;
+        int Difference = 10;
+        int Quotient = 3;
+        int Modulo = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        double doubleOperandA = 5.05;
+        double doubleOperandB = 2.05;
+        double doubleSum = 7.1;
+        double doubleDifference = 3;
+        double doubleProduct = 10.5;
+        double doubleQoutient = 2.46;
+
+        System.out.println("The sum using ints of" + intOperandA + intOperandB + "is" + 20);
+        System.out.println("int Product = intOperandA * intOperandB");
+        System.out.println("int Difference = intOperandA - intOperandB");
+        System.out.println("The quotient using ints of" + intOperandA + intOperandB + "is" + 3);
+        System.out.println("int Modulo = intOperandA % intOperandB");
+
+        System.out.println("The sum of the ints of" + doubleOperandA + doubleOperandB + "is" + doubleSum);
+        System.out.println("doubleProduct = doubleOperandA * doubleOperandB");
+        System.out.println("The difference of the ints of" + doubleOperandA + doubleOperandB + "is" + 3);
+        System.out.println("doubleQoutient = doubleOperandA / doubleOperandB");
+
+        int myGasMoney = 20; // I wish :(
+        int myFamilyMembers = 5;
+        int MyNumberOfAssignments = 15;
+        int myAverageErrors = 10;
+        int myPosters = 3;
+        int myFavoriteNumber = 0;
+
+        double myShoeSize = 10.5;
+        double myCousinsAge = 7.1;
+        double myDogsAge = 3;
+        double myDrinkCost = 2.46;
+        double myCordLegth = 2.05;
+        double myChange = 5.05;
+        
     }
 }
